66895c42c13fdc5256ff5d2f1100f9ee992d89cc
dthomas-hkd <david_thomas@hakkoda.io>
2023-04-01 12:32:43 -0600

    Initial commit

---
+++ Weather.py
@@ -0,0 +1,115 @@
+from datetime import timezone
+from snowflake.snowpark import Session
+from snowflake.snowpark.types import VariantType,  StructField, StructType
+from snowflake.snowpark.functions import col
+from utils import snowpark_utils
+import pandas as pd , urllib.request, json, re, sys, os
+
+# Get Public IP
+
+
+# def getPublicIP():
+#     data = str(urllib.request.urlopen('http://checkip.dyndns.com/').read())
+#     return re.compile(r'Address: (\d+.\d+.\d+.\d+)').search(data).group(1)
+
+# IP = str(getPublicIP())
+
+# # Get Location
+# url = 'http://ipinfo.io/' + IP + '/json'
+# response = urllib.request.urlopen(url)
+# data_ip = json.load(response)
+# city = data_ip['city']
+# location = data_ip['loc']
+
+# lat = location.split(",")[0]
+# lon = location.split(",")[1]
+
+# print(lat)
+# print(lon)
+
+
+# print(data_ip)
+# print(city)
+
+def obtain_CR_prov_ids () -> list:
+
+    with open('city_list.json') as file:
+        data = json.load(file)
+
+    df = pd.DataFrame(data)
+    
+    CR_df = df[df["country"] == "CR"]
+
+    print("\n\nAvailable CR cities")
+    print(CR_df)
+
+    CR_prov_df = CR_df[CR_df["name"].str.contains("Provincia de")]
+
+    print("\n\nCR provinces")
+
+    print(CR_prov_df)
+
+    CR_prov_df['id'] = CR_prov_df['id'].astype(int)
+
+    return CR_prov_df
+
+def obtain_city_data(CR_prov_df):
+
+    data = []   
+    for index, row in CR_prov_df.iterrows():
+        
+        city_name = row['name']
+
+        city_id =  row['id']
+
+        print(f"\n\nEvaluating {city_name}, id {city_id}")
+
+        # Get Weather
+        response_current = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={api_key}&units=metric')
+
+        data_weather_current = json.load(response_current)
+
+        print ("\nCurrent Weather : " + str(data_weather_current['main']['temp']) + "C")
+
+        json_object = json.dumps(data_weather_current)
+
+        print(json_object)
+
+        data.append(json_object.replace(" ", ""))
+
+    data_df = pd.DataFrame({'data': data})
+    
+    return(data_df)
+
+
+def upload_raw_data (data_df):
+
+    session = snowpark_utils.get_snowpark_session()
+       
+    session.write_pandas(data_df, "raw_weather_2", quote_identifiers= False, auto_create_table=True, table_type = "temp")
+
+    cleaned_raw_data = session.sql("select parse_json(data) as data from raw_weather_2")
+    
+    cleaned_raw_data.write.mode("append").save_as_table("cleaned_raw_weather")
+
+    upload_results = session.table("cleaned_raw_weather").collect()
+
+    print()
+    print("cleaned_raw_weather current content:")
+    for r in upload_results:
+        print()
+        print(r)
+
+if __name__ == "__main__":
+
+    current_dir = os.getcwd()
+    parent_dir = os.path.dirname(current_dir)
+    sys.path.append(parent_dir)
+  
+    api_key= os.environ["api_key"]
+    
+    CR_prov_df = obtain_CR_prov_ids()
+
+    data_df = obtain_city_data(CR_prov_df)
+
+    upload_raw_data(data_df)
