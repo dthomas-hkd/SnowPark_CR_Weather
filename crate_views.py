c6115c38c6de43329b8f6232c624f217474c0322
dthomas-hkd <david_thomas@hakkoda.io>
2023-04-01 20:48:39 -0600

    created views

--- crate_views.py
+++ crate_views.py
@@ -1,6 +1,7 @@
 from snowflake.snowpark import Session
+from snowflake.snowpark.functions import col, call_udf
 from utils import snowpark_utils
-
+from unidecode import unidecode
 
 session = snowpark_utils.get_snowpark_session()
 
@@ -8,6 +9,19 @@ weather_table = session.table("Weather")
 
 print(weather_table.count())
 
-provinces = weather_table.select("city_name").distinct().collect()
+provinces_df = weather_table.select("city_name").distinct().collect()
 
-print(provinces)
+provinces = []
+
+for province in provinces_df:
+    print("Processing "+province[0])
+
+    province_name = province[0].split("Provincia de ")[-1]
+    province_name_uni = unidecode( province_name.replace(" ", "_") ).upper()
+
+    province_data = weather_table.filter(col ("city_name") == province[0] )
+    province_data = province_data.with_column("date", call_udf("UTC_TO_TZ",col("date")))
+
+    result = province_data.createOrReplaceView("WEATHER_"+province_name_uni)
+    
+    print(result[0][0])
