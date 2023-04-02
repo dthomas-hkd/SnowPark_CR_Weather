4fc13180222e51b9982404a4fcce619930e445ca
dthomas-hkd <david_thomas@hakkoda.io>
2023-04-01 20:48:31 -0600

    created views, updated sql

--- select_from_weather.sql
+++ select_from_weather.sql
@@ -1,11 +1,14 @@
---CREATE VIEWS PER PROVINCE
-
+--UPDATE WEATHER TABLE AND SELECT DATA
 USE WAREHOUSE COMPUTE_WH;
 USE SCHEMA HLB_TEST.PUBLIC;
 
 CALL INSERT_INTO_WEATHER();
 
-SELECT UTC_TO_TZ(date), city_name, weather_main, weather_description, temp, feels_like, temp_min, temp_max, pressure, humidity, clouds, wind_speed, visibility
-FROM WEATHER order by 1 desc;
+SELECT UTC_TO_TZ(date), date, city_name, weather_main, weather_description, temp, feels_like, temp_min, temp_max, pressure, humidity, clouds, wind_speed, visibility
+FROM WEATHER order by 3,  1 desc;
 
+
+-- SELECT FROM VIEW WEATHER_HEREDIA
+
+SELECT * FROM WEATHER_HEREDIA;
 
