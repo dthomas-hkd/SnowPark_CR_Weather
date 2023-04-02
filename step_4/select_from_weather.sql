631d7b59ac06ebabc70c0b5dbd6bb2cb06090677
dthomas-hkd <david_thomas@hakkoda.io>
2023-04-02 00:17:33 -0600

    updated views, udf and sproc

--- step_4/select_from_weather.sql
+++ step_4/select_from_weather.sql
@@ -2,9 +2,13 @@
 USE WAREHOUSE WEAThER_WH;
 USE SCHEMA CR_WEATHER.WEATHER;
 
+USE ROLE WEATHER_ROLE;
+
+
 CALL INSERT_INTO_WEATHER();
 
-SELECT UTC_TO_TZ(date), date, city_name, weather_main, weather_description, temp, feels_like, temp_min, temp_max, pressure, humidity, clouds, wind_speed, visibility
+
+SELECT utc_to_tz(date), date, city_name, weather_main, weather_description, temp, feels_like, temp_min, temp_max, pressure, humidity, clouds, wind_speed, visibility
 FROM FINAL_WEATHER order by 3,  1 desc;
 
 
